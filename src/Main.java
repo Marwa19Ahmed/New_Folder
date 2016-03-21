e1de654936857206323ec4321fe2707985d37b9d
Mohamed Nasser <Mohamed Nasser@DESKTOP-AU7I50Q>
2016-03-21 14:56:27 +0200

    Fun

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void fun()
+	{
 		System.out.print("Hello Mohamed :)");
-
+	}
+	public static void main(String[] args) {
+		
+		fun();
 	}
 
 }
